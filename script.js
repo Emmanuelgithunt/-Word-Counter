c8e9a48e1d2a7aa8dc42e87926e01897ea30eec3
Emmanuelgithunt <emma2004ife@gmail.com>
2023-09-28 10:31:43 +0100

    targeting the IDs in javascript

---
+++ script.js
@@ -0,0 +1,4 @@
+const input = document.getElementById("input-text");
+const word = document.getElementById("word-count");
+const character = document.getElementById("character-count");
+const unique = document.getElementById("unique-words");
