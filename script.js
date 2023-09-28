95001bdbace7e4ce5af307722ebbf98bfeca75d8
Emmanuelgithunt <emma2004ife@gmail.com>
2023-09-28 11:24:10 +0100

    counter complete

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const input = document.getElementById("input-text");
-const word = document.getElementById("word-count");
-const character = document.getElementById("character-count");
-const unique = document.getElementById("unique-words");
+window.addEventListener('DOMContentLoaded', ()=>{
+    const input = document.getElementById("input-text");
+    const word = document.getElementById("word-count");
+    const charact = document.getElementById("character-count");
+    const uniqueword = document.getElementById("unique-words");
+
+    input.addEventListener('input',()=>{
+        const textValue = input.value.trim();
+        const words = textValue === '' ? 0 : textValue.split(/\s+/).length;
+        const characters = textValue.length;
+        const unique = new Set(textValue.match(/\b\w+\b/g)).size;
+
+        word.innerHTML = 'Word count:' + words;
+
+        charact.innerHTML = 'Character count(including spaces): ' + characters;
+        
+        uniqueword.innerHTML = 'Unique words: ' + unique;
+    })
+})
